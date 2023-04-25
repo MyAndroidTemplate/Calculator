86d93b5ac455a357241421ee813db11d6e7608a2
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2023-04-25 06:48:58 +0300

    Add Display Module

---
+++ app/src/main/java/com/example/modules/DisplayShow.java
@@ -0,0 +1,20 @@
+package com.example.modules;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.calculator.R;
+
+public class DisplayShow{
+    View view = null;
+    TextView display = null;
+    public DisplayShow(View view) {
+        this.view = view;
+        this.display = view.findViewById(R.id.display);
+    }
+
+    public void setDisplayValue(String value)
+    {
+        display.setText(value);
+    }
+}
